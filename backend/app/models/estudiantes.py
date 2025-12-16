56e92e5ba4282123ef53a479f229edda8e07f2a5
didierguerreros <didierguerreros@gmail.com>
2025-12-15 20:26:04 -0500

    servidor ok
    server ok

--- backend/app/models/estudiantes.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class Estudiante(Base):
-    __tablename__ = "estudiantes"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, nullable=False)
-    curso = Column(String, nullable=False)
